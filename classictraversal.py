10cb00d841f31cc00a1ffa1c87f85d9a1db57bf6
Joost Rijneveld <joost@joostrijneveld.nl>
2015-08-18 16:10:55 +0200

    Properly keep track of completed Treehash

--- classictraversal.py
+++ classictraversal.py
@@ -55,28 +55,31 @@ def treehash(h):
 
 class Treehash(object):
 
-    def __init__(self, startidx=0):
+    def __init__(self, h, startidx=0, completed=False):
         self.next_idx = startidx
         self.stack = []
-        self.node = None
+        self.completed = completed
+        self.h = h
 
     def update(self):
-        if self.node and self.stack and self.stack[-1].h == self.node.h:
-            node2 = self.stack.pop()
-            self.node = Node(h=self.node.h+1, v=g(node2.v + self.node.v))
+        if self.completed:
             return
-        elif self.node:
-            self.stack.append(self.node)
+        if len(self.stack) >= 2 and self.stack[-1].h == self.stack[-2].h:
+            node_r = self.stack.pop()
+            node_l = self.stack.pop()
+            self.stack.append(Node(h=node_l.h + 1, v=g(node_l.v + node_r.v)))
+        else:
+            self.stack.append(Node(h=0, v=leafcalc(self.next_idx)))
             self.next_idx += 1
-        if self.next_idx >= 2 ** H:
-            return  # this is a leftover update cycle at 2^h
-        self.node = Node(h=0, v=leafcalc(self.next_idx))
+        if self.stack[-1].h == self.h:
+            self.completed = True
+        return
 
 
 def keygen_and_setup():
     """Sets up TREEHASH and AUTH for the start of classic_merkle_traversal."""
     for h in range(H):
-        TREEHASH[h] = Treehash()
+        TREEHASH[h] = Treehash(h, completed=True)
     stack = []
     for j in range(2 ** H):
         node1 = Node(h=0, v=leafcalc(j))
@@ -104,7 +107,8 @@ def classic_merkle_traversal(s):
         if TREEHASH[h].stack:  # prevent going past 2^H'th leaf node
             AUTH[h] = TREEHASH[h].stack[0]
             startidx = (s + 1 + 2 ** h) ^ (2 ** h)
-            TREEHASH[h].__init__(startidx)
+            if startidx < 2 ** H:
+                TREEHASH[h].__init__(h, startidx)
 
     # and build stacks
     for h in range(H):
@@ -129,7 +133,7 @@ def compute_root(idx, authpath):
 if __name__ == "__main__":
     correct_root = recursive_hash(H)
     print('Treehash function: {}'.format(treehash(H).v == correct_root))
-    th = Treehash()
+    th = Treehash(H)
     for _ in range(2 ** (H + 1)):
         th.update()
     print('Treehash class: {}'.format(th.stack[0].v == correct_root))
